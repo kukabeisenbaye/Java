d0079db3e437bd15c469de534617c7105962ce82
Kuanysh <148076048+kukabeisenbaye@users.noreply.github.com>
2024-12-10 23:47:48 +0600

    Initial commit with code

---
+++ rk2/rk2/src/test/java/com/example/rk2/Rk2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rk2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rk2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
